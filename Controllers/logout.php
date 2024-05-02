96ca7ee40e9454f26055e9a98e20b07fca17040d
RawdaYassin <129060285+RawdaYassin@users.noreply.github.com>
2024-05-02 19:17:14 +0300

    Delete Controllers directory

--- Controllers/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('../Models/User.php'); // Assuming User class is in Models directory
-
-// No need for additional code here as the logout functionality resides in the User class
-
-?>
